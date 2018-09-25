622f001ceefd679654bdf9b28f44212d58f13340
Reed <reedcw1@vanderbilt.edu>
2018-09-25 11:59:45 -0500

    testing ethiopia plugin

--- combineCSVs/index.php
+++ combineCSVs/index.php
@@ -56,6 +56,9 @@ if (isset($_GET['submit']) && (isset($_POST['project_id'])) && ($_POST['project_
         $dateParts = explode('-', $procDate);
         $year = $dateParts[0];
         if(strlen($year) == 4){
+			header('content-type: text/plain');
+			print_r(json_encode([$fields]));
+			exit;
             $response = REDCap::saveData($projectId, 'json', json_encode([$fields]));
             $errors = $response['errors'];
 
@@ -124,12 +127,9 @@ if (isset($_GET['submit']) && (isset($_POST['project_id'])) && ($_POST['project_
                 if ($j === 0) {
                     if (!$hasHeaderRowBeenWritten) {
                         $hasHeaderRowBeenWritten = true;
-						echo "writing headers for test:\n";
                         for ($k =0; $k < count($row); $k++) {
                             $headers[] = $row[$k];
-							echo $row[$k] . "\n";
                         }
-						echo $row[$k] . "\n";
 						
                         $handleHeaders($headers);
                     }
